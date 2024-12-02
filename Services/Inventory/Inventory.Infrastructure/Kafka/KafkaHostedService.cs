9fa549b4d5e6db373e109346984b7e5c16d1ed65
Youssef-Malek2004 <youssefmmalek@gmail.com>
2024-12-02 13:16:38 +0200

    Changing Folder Structure-Inventory

--- Services/Inventory/Inventory.Infrastructure/Kafka/KafkaHostedService.cs
+++ Services/Inventory/Inventory.Infrastructure/Kafka/KafkaHostedService.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.Extensions.Hosting;
 
 namespace Inventory.Infrastructure.Kafka;
---
+++ Services/Inventory/Inventory.Infrastructure/Migrations/20241201124805_Fixing_Name.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fixing_Name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Inventory");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Inventory",
+                table: "Inventory",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Inventory",
+                table: "Inventory");
+
+            migrationBuilder.RenameTable(
+                name: "Inventory",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
