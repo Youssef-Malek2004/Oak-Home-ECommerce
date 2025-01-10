351318f2c23f6c9d21eee68ba25000b24e5a18e4
Youssef-Malek2004 <youssefmmalek@gmail.com>
2025-01-10 20:21:35 +0200

    Adding Speed Insights

--- Frontend/EcommerceUI/src/main.tsx
+++ Frontend/EcommerceUI/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./Components/Contexts/Authentication/Authentication.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
+      <SpeedInsights />
       <App />
     </AuthProvider>
   </StrictMode>
