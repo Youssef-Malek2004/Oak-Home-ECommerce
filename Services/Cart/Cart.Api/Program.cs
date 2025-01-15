19cc143a7896e19df6725585c627866a30683b7d
Youssef-Malek2004 <youssefmmalek@gmail.com>
2025-01-15 18:27:40 +0200

    Program.cs Updates

--- Services/Cart/Cart.Api/Program.cs
+++ Services/Cart/Cart.Api/Program.cs
@@ -3,6 +3,10 @@ using Cart.Api.Middlewares;
 using Cart.Api.OptionsSetup;
 using Cart.Infrastructure;
 using Cart.Infrastructure.Kafka;
+using Cart.Application.CQRS.Commands.CreateCart;
+using Cart.Application.Services.Redis;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 
 using Shared.Contracts.Entities.NotificationService;
 using Shared.Contracts.Kafka;
@@ -13,11 +17,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Configuration.AddEnvironmentVariables();
 
-builder.Configuration["ConnectionStrings:DatabaseLocal"] = 
+builder.Configuration["ConnectionStrings:DatabaseLocal"] =
     Environment.GetEnvironmentVariable("DATABASE_LOCAL") ?? builder.Configuration["ConnectionStrings:DatabaseLocal"];
-builder.Configuration["ConnectionStrings:DatabaseDocker"] = 
+builder.Configuration["ConnectionStrings:DatabaseDocker"] =
     Environment.GetEnvironmentVariable("DATABASE_DOCKER") ?? builder.Configuration["ConnectionStrings:DatabaseDocker"];
-builder.Configuration["UsingDocker"] = 
+builder.Configuration["UsingDocker"] =
     Environment.GetEnvironmentVariable("USING_DOCKER") ?? builder.Configuration["UsingDocker"];
 
 var usingAspire = Environment.GetEnvironmentVariable("Using__Aspire");
@@ -34,11 +38,29 @@ builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
 
 builder.Services.Configure<KafkaSettings>(builder.Configuration.GetSection("KafkaSettings"));
 builder.Services.AddKafkaAdminClient();
-builder.Services.AddSingleton<IKafkaProducerService,KafkaProducerService>();
+builder.Services.AddSingleton<IKafkaProducerService, KafkaProducerService>();
 builder.Services.AddSingleton<IKafkaConsumerService, KafkaConsumerService>();
 builder.Services.AddSingleton<IKafkaNotificationService, KafkaNotificationsService>();
 builder.Services.AddSingleton<KafkaEventProcessor>();
 builder.Services.AddSingleton<KafkaDispatcher>();
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssemblyContaining<CreateCartCommandHandler>());
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    options.InstanceName = "CartService_";
+});
+
+builder.Services.AddMemoryCache();
+builder.Services.AddScoped<IRedisService, RedisService>();
+
+builder.Services.Configure<DistributedCacheEntryOptions>(options =>
+{
+    options.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+    options.SlidingExpiration = TimeSpan.FromMinutes(20);
+});
 
 builder.Services.AddHostedService<KafkaInitializationHostedService>();
 builder.Services.AddHostedService<KafkaHostedService>();
@@ -61,7 +83,7 @@ var endpoints = app.MapGroup("api");
 
 endpoints.MapGet("testing", (HttpResponse response, HttpContext context) =>
 {
-    response.WriteAsJsonAsync("Here" + context.Items["VendorId"] );
+    response.WriteAsJsonAsync("Here" + context.Items["VendorId"]);
 });
 
 app.AddLifetimeEvents();
